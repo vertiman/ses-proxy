8eb2e03acff798454925a3c76f2b68de81b1468b
Mike Wertman <mike@resolver.com>
2017-09-06 20:27:41 -0400

    a bit of clean up

--- SesSender.js
+++ SesSender.js
@@ -1,72 +1,72 @@
 var fs = require('fs'),
-  async = require('async'),
-  AWS = require('aws-sdk'),
-  proxyAgent = require('proxy-agent'),
-  path = require('path');
+    async = require('async'),
+    AWS = require('aws-sdk'),
+    proxyAgent = require('proxy-agent'),
+    path = require('path');
 
 function SesSender(opts) {
-  var credentialsFilePath = './ses-credentials.json';
-  if (opts.config) {
-    credentialsFilePath = opts.config;
-  }
+    var credentialsFilePath = './ses-credentials.json';
+    if (opts.config) {
+        credentialsFilePath = opts.config;
+    }
 
-  if (fs.existsSync(credentialsFilePath)) {
-    AWS.config.loadFromPath(credentialsFilePath);
-  } else if (fs.existsSync(path.join(process.cwd(), credentialsFilePath))) {
-    AWS.config.loadFromPath(path.join(process.cwd(), credentialsFilePath));
-  } else {
-    console.warn('Warning: Can not find credentials file.')
-  }
+    if (fs.existsSync(credentialsFilePath)) {
+        AWS.config.loadFromPath(credentialsFilePath);
+    } else if (fs.existsSync(path.join(process.cwd(), credentialsFilePath))) {
+        AWS.config.loadFromPath(path.join(process.cwd(), credentialsFilePath));
+    } else {
+        console.warn('Warning: Can not find credentials file.')
+    }
 
-  var proxy = process.env.https_proxy;
-  proxy = opts.proxy ? opts.proxy : proxy;
+    var proxy = process.env.https_proxy;
+    proxy = opts.proxy ? opts.proxy : proxy;
 
-  if (proxy) {
-    console.log('Using https proxy', proxy);
-    AWS.config.update({
-      httpOptions: {
-        agent: proxyAgent(proxy, true)
-      }
-    });
-  }
+    if (proxy) {
+        console.log('Using https proxy', proxy);
+        AWS.config.update({
+            httpOptions: {
+                agent: proxyAgent(proxy, true)
+            }
+        });
+    }
 
-  this.messageQueue = async.queue(this.processClient.bind(this), 1);
+    this.messageQueue = async.queue(this.processClient.bind(this), 1);
 
 }
 
 SesSender.prototype = {
 
-  queue: function(client) {
-    this.messageQueue.push(client);
-  },
+    queue: function (client) {
+        this.messageQueue.push(client);
+    },
 
-  processClient: function(client, callback) {
-    this.sendRaw(client, callback);
-  },
+    processClient: function (client, callback) {
+        this.sendRaw(client, callback);
+    },
 
-  sendRaw: function(client, callback) {
+    sendRaw: function (client, callback) {
 
-    var ses = new AWS.SES();
+        var ses = new AWS.SES();
 
-    var options = {
-      RawMessage: {
-        Data: client.data
-      },
-      Destinations: client.to
-    };
+        var options = {
+            RawMessage: {
+                Data: client.data
+            },
+            Destinations: client.to
+        };
 
-    console.log('Attempting to send SES message');
-    ses.sendRawEmail(options, function(err, data) {
-      if (err) {
-        console.error('Error sending SES email', err);
-        console.error(err.stack);
-        console.log(this.httpResponse && this.httpResponse.body && this.httpResponse.body.toString());
-      } else {
-        console.log('Successfully sent SES email.', data);
-      }
-      callback();
-    });
-  }
+        console.log('Attempting to send SES message');
+        ses.sendRawEmail(options, function (err, data) {
+            if (err) {
+                console.error('Error sending SES email', err);
+                console.error(err.stack);
+                console.log(this.httpResponse && this.httpResponse.body && this.httpResponse.body.toString());
+            } else {
+                console.log('Successfully sent SES email.', data);
+            }
+            callback();
+        });
+    }
 }
 
 module.exports = SesSender;
--- tests/test-send-email.js
+++ tests/test-send-email.js
@@ -1,60 +1,51 @@
 var should = require('should'),
-  nodemailer = require('nodemailer'),
-  smtpTransport = require('nodemailer-smtp-transport');
-
-
-
-
+    nodemailer = require('nodemailer'),
+    smtpTransport = require('nodemailer-smtp-transport');
 
 describe('Send Email', function() {
-  this.timeout(60000);
+    this.timeout(60000);
 
-  before(function(done) {
-    var server = require('../index.js');
-    server(2525);
-    setTimeout(done, 200);
-  });
-  after(function(done) {
-    //Wait a bit for the SES api call.
-    setTimeout(done, 15000);
-  });
-
-  it('should send an email successfully', function(testsDone) {
-
-
-
-
-    // create reusable transporter object using SMTP transport
-    var transporter = nodemailer.createTransport(smtpTransport({
-      host: 'localhost',
-      port: 2525,
-      // debug: true
-    }));
-
-    //Turn this on with the debug:true in smtpTransport
-    transporter.on('log', function(data) {
-      console.log(data);
-    })
-
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-      from: process.env.FROM ? process.env.FROM : 'lisa@example.com',
-      to: process.env.TO ? process.env.TO : 'home@example.com',
-      subject: 'Hello ✔',
-      text: 'Hello\nworld ✔',
-      html: '<b>Hello<br>world ✔</b>'
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info) {
-      should(error).not.be.ok;
-      should(info).be.ok;
-      info.should.have.property('response');
-
-      testsDone(error);
+    before(function(done) {
+        var server = require('../index.js');
+        server(2525);
+        setTimeout(done, 200);
+    });
+    after(function(done) {
+        //Wait a bit for the SES api call.
+        setTimeout(done, 15000);
     });
 
+    it('should send an email successfully', function(testsDone) {
+        // create reusable transporter object using SMTP transport
+        var transporter = nodemailer.createTransport(
+            smtpTransport({
+                host: 'localhost',
+                port: 2525
+                // debug: true
+            })
+        );
 
-  });
+        //Turn this on with the debug:true in smtpTransport
+        transporter.on('log', function(data) {
+            console.log(data);
+        });
 
+        // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: process.env.FROM ? process.env.FROM : 'mike@resolver.com',
+            to: process.env.TO ? process.env.TO : 'success@simulator.amazonses.com',
+            subject: 'Hello ✔',
+            text: 'Hello\nworld ✔',
+            html: '<b>Hello<br>world ✔</b>'
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function(error, info) {
+            should(error).not.be.ok;
+            should(info).be.ok;
+            info.should.have.property('response');
+
+            testsDone(error);
+        });
+    });
 });
